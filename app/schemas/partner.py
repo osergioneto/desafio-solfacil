64d4df545bd4f821abcca7c1084d11d1561eac0a
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-06-25 01:27:49 -0300

    feat: add BasePartner, PartnerCreate, PartnerUpdate, PartnerInDB and Partner

--- app/schemas/partner.py
+++ app/schemas/partner.py
@@ -1,17 +1,32 @@
-from uuid import UUID
+# from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel
 
-class Partner(BaseModel):
-    id: UUID
+from pydantic import BaseModel, EmailStr
+
+class BasePartner(BaseModel):
+    email: EmailStr
     cnpj: str
     company_name: str
     trading_name: str
     telephone: str
-    email: str
     zip_code: str
-    created_at: datetime
+
+class PartnerCreate(BasePartner):
+    pass
+
+
+class PartnerUpdate(BasePartner):
     updated_at: datetime
+
+
+class PartnerInDB(BasePartner):
+    id: int = None
+    created_at: datetime = None
+    updated_at: datetime = None
 
     class Config:
         orm_mode = True
+
+
+class Partner(PartnerInDB):
+    pass
