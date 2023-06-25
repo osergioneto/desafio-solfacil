37e164bc298904fd0ebf22707e9e1e374a2a8268
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-06-25 08:11:16 -0300

    feat: add functions to parse, validate and create/update partners from csv

--- app/services/partner.py
+++ app/services/partner.py
@@ -1,50 +1,105 @@
-import csv
+import copy, csv, re
 from sqlalchemy.orm import Session
 from app.crud import crud_partner
+from datetime import datetime
+from app.schemas import BasePartner
 
-def from_csv(file: bytes, db: Session): 
-    csv_data = file.decode() 
-    reader = csv.reader(csv_data.splitlines()) 
+def upsert_from_csv(file: bytes, db: Session): 
+    csv_data = file.decode('utf-8') 
+
+    validated_csv = validate_csv(csv_data)
+    if validated_csv["errors"]:
+        file_path = '../assets/output.csv'
+        write_errors(validated_csv["rows"], file_path)
+
+        return file_path
+        
+
     partners = []
+    for row in csv.DictReader(csv_data.splitlines()):
+        partner_obj = format_partner(row)
+        partner = crud_partner.find_partner_by_cnpj_or_email(db, cnpj=partner_obj.cnpj, email=partner_obj.email)
+        if partner:
+            partner = crud_partner.update_partner(db, db_obj=partner, obj_in={**partner_obj.dict(),"updated_at": datetime.now()})
+        else:
+            partner = crud_partner.create_partner(db, partner={partner_obj})
+        partners.append(partner)
 
-    next(reader)
+    return partners
+
+def validate_csv(csv_data: str):
+    reader = csv.DictReader(csv_data.splitlines()) 
+
+    rows = []
+    all_errors = []
 
     for row in reader:
-        cnpj = row[0]
-        company_name = row[1]
-        trading_name = row[2]
-        telephone = row[3]
-        email = row[4]
-        zip_code = row[5]
+        errors = []
+        for fieldname, value in row.items():
+            if not value:
+                errors.append(f"{fieldname} is empty.")
 
-        partner = crud_partner.find_partner_by_cnpj_or_email(db, cnpj=cnpj, email=email)
-        # print(partner.__dict__)
-        if partner:
-            print("Encontrou partner")
-            partner = crud_partner.update_partner(db, db_obj=partner, obj_in={
-                "cnpj": cnpj, 
-                "email": email, 
-                "company_name": company_name,
-                "trading_name": trading_name, 
-                "telephone": telephone, 
-                "zip_code": zip_code
-            })
-            partners.append(partner)
-            print("Atualizou partner")
-            print("partners", partners)
-        else:
-            print("Não achou")
-            partner = crud_partner.create_partner(db, partner={
-                "cnpj": cnpj, 
-                "email": email, 
-                "company_name": company_name,
-                "trading_name": trading_name, 
-                "telephone": telephone, 
-                "zip_code": zip_code
-            })
-            print("Criou partner", partner)
-            partners.append(partner)
-            print("partners", partners)
-        # print(row)
-    print("partners", partners)
-    return partners
+            if fieldname == "CNPJ" and value and not validate_cnpj(value):
+                errors.append(f"Invalid CNPJ: {value}")
+
+            if fieldname == "Telefone" and value and not validate_telefone(value):
+                errors.append(f"Invalid telefone: {value}")
+
+            if fieldname == "CEP" and value and not validate_cep(value):
+                errors.append(f"Invalid CEP: {value}")
+
+            if fieldname == "Email" and value and not validate_email(value):
+                errors.append(f"Invalid email: {value}")
+
+        row["Erros"] = ", ".join(errors)
+        all_errors.append(errors)
+
+        rows.append(row)
+   
+    return {"rows": rows, "errors": sum(all_errors, [])}
+
+def write_errors(data, file_path):
+    fieldnames = data[0].keys()
+
+    with open(file_path, 'w', newline='') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        return csv_writer.writerows(data)
+
+def validate_cnpj(cnpj):
+    # CNPJ pattern: xx.xxx.xxx/xxxx-xx
+    pattern = r"^(\d{2}.?\d{3}.?\d{3}\/?\d{4}\-?\d{2})$"
+    return re.fullmatch(pattern, cnpj) is not None
+
+def validate_telefone(telefone):
+    # Telefone pattern: (xx) xxxxx-xxxx
+    pattern = r"\(\d{2}\) \d{5}-\d{4}"
+    return re.fullmatch(pattern, telefone) is not None
+
+def validate_cep(cep):
+    # CEP pattern: xxxxx-xxx
+    pattern = r"(^\d{5})\-?(\d{3}$)"
+    return re.fullmatch(pattern, cep) is not None
+
+def validate_email(email):
+    # Email pattern validation using a simple regex pattern
+    pattern = r"[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+"
+    return re.fullmatch(pattern, email) is not None
+
+def format_partner(row: list[str]):
+    cnpj = row['CNPJ']
+    company_name = row['Razão Social']
+    trading_name = row['Nome Fantasia']
+    telephone = row['Telefone']
+    email = row['Email']
+    zip_code = row[' CEP']
+
+    return BasePartner(
+        cnpj = cnpj, 
+        email = email, 
+        company_name = company_name,
+        trading_name = trading_name, 
+        telephone = telephone, 
+        zip_code = zip_code
+    )
