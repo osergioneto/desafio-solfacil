f5b2730a8c7ad9616d89ecf8602e9c21149f1f94
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-06-26 11:54:27 -0300

    test: add tests to validate csv

---
+++ app/tests/services/test_partner.py
@@ -0,0 +1,68 @@
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from app.tests.utils.db import clean
+from app.services import partner
+
+def test_create_from_function_upsert_from_csv(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n12.473.742/0001-13,Sol Forte,Sol Forte LTDA,(11) 98207-9903,atendimento@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,atendimento@soldamanha.com,04127-000"
+    partners = partner.upsert_from_csv(file_str.encode(), db=db)
+
+    assert len(partners) == 2
+    assert partners[0].cnpj == "12.473.742/0001-13"
+    assert partners[1].cnpj == "19.478.819/0001-97"
+
+def test_update_from_function_upsert_from_csv(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n12.473.742/0001-13,Sol Forte,Sol Forte LTDA,(11) 98207-9903,atendimento@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,atendimento@soldamanha.com,04127-000"
+    # First step create the partners
+    partner.upsert_from_csv(file_str.encode(), db=db)
+    # Second step update the partners
+    partners = partner.upsert_from_csv(file_str.encode(), db=db)
+
+    assert len(partners) == 2
+    assert partners[0].updated_at != None
+    assert partners[1].updated_at != None
+
+def test_validate_csv_with_wrong_email(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n12.473.742/0001-13,Sol Forte,Sol Forte LTDA,(11) 98207-9903,@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,wrongemail,04127-000"
+    partners = partner.validate_csv(file_str)
+
+    assert partners["errors"] == ['Invalid email: @solforte.com', 'Invalid email: wrongemail']
+
+def test_validate_csv_with_wrong_cnpj(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n530.774.460-90,Sol Forte,Sol Forte LTDA,(11) 98207-9903,atendimento@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,atendimento@soldamanha.com,04127-000"
+    partners = partner.validate_csv(file_str)
+
+    assert partners["errors"] == ['Invalid CNPJ: 530.774.460-90']
+
+def test_validate_csv_with_wrong_telephone(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n12.473.742/0001-13,Sol Forte,Sol Forte LTDA,132,atendimento@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 222,atendimento@soldamanha.com,04127-000"
+    partners = partner.validate_csv(file_str)
+
+    assert partners["errors"] == ['Invalid telefone: 132', 'Invalid telefone: (21) 222']
+
+def test_validate_csv_with_wrong_zipcode(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n12.473.742/0001-13,Sol Forte,Sol Forte LTDA,(11) 98207-9903,atendimento@solforte.com,04127\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,atendimento@soldamanha.com,200"
+    partners = partner.validate_csv(file_str)
+
+    assert partners["errors"] == ['Invalid CEP: 04127', 'Invalid CEP: 200']
+
+def test_validate_csv_with_missing_data(client: TestClient, db: Session):
+    clean(db)
+    
+    file_str = "CNPJ,Razão Social,Nome Fantasia,Telefone,Email, CEP\n,Sol Forte,,,atendimento@solforte.com,04127-000\n19.478.819/0001-97,Sol da Manhã,Sol da Manhã LTDA,(21) 98207-9902,atendimento@soldamanha.com,"
+    partners = partner.validate_csv(file_str)
+
+    assert partners["errors"] == ['CNPJ is empty.', 'Nome Fantasia is empty.', 'Telefone is empty.', ' CEP is empty.']
+    
