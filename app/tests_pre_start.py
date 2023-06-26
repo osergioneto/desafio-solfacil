3228c655c373e1184dcdbdbab731046c4f0c4ab9
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-06-26 11:47:19 -0300

    chore: add scripts to execute tests

---
+++ app/tests_pre_start.py
@@ -0,0 +1,40 @@
+import logging
+from sqlalchemy import text
+from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
+
+from app.tests.crud.db import session, init_db
+# from app.tests.crud.db import i
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+max_tries = 60 * 5  # 5 minutes
+wait_seconds = 1
+
+
+@retry(
+    stop=stop_after_attempt(max_tries),
+    wait=wait_fixed(wait_seconds),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def init() -> None:
+    try:
+        # Try to create session to check if DB is awake
+        db = session.TestingSessionLocal()
+        sql_expression = text('SELECT 1')
+        db.execute(sql_expression)
+        # init_db()
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+
+def main() -> None:
+    logger.info("Initializing service")
+    init()
+    logger.info("Service finished initializing")
+
+
+if __name__ == "__main__":
+    main()
