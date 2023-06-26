8d6cb3857520e7d261063c56bf63976bb29d17e7
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-06-26 11:42:42 -0300

    refactor: remove unused endpoint. Return status 400 when file is not correct

--- app/api/endpoints/partners.py
+++ app/api/endpoints/partners.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, File, HTTPException
+from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
 from typing import Annotated
@@ -10,21 +11,11 @@ from app.services import partner
 
 router = APIRouter()
 
-@router.get("/")
-def root():
-    return {"message": "Hello World"}
-
 @router.get("/partners/", response_model=list[schemas.Partner] | None)
 def read_partners(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     partners = crud_partner.get_partners(db, skip=skip, limit=limit)
     return partners
 
-@router.post("/partners/", response_model=schemas.Partner)
-def create_partner(partner: schemas.Partner, db: Session = Depends(get_db)):
-    db_partner = crud_partner.find_partner_by_cnpj_or_email(db, cnpj=partner.cnpj, email=partner.email)
-    if db_partner:
-        raise HTTPException(status_code=409, detail="Partner already registered")
-    return crud_partner.create_partner(db=db, partner=partner)
 
 @router.post("/bulk-partners/", response_model=list[schemas.Partner] | None)
 def create_partners(file: Annotated[bytes, File()], db: Session = Depends(get_db)):
@@ -32,4 +23,4 @@ def create_partners(file: Annotated[bytes, File()], db: Session = Depends(get_db
     if isinstance(parterns_or_file, list):
         return parterns_or_file
     
-    return FileResponse(parterns_or_file)
+    return FileResponse(parterns_or_file, status_code=400)
